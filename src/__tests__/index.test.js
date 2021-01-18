ac745a82d07351f79c501751b1cc66263a6425dc
Michael Matloka <dev@twixes.com>
2021-01-18 15:16:22 +0100

    Add working tests

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,62 @@
+const { createEvent, createIdentify, getMeta, resetMeta, clone } = require('@posthog/plugin-scaffold/test/utils')
+
+const { processEvent } = require('..')
+
+global.fetch = jest.fn(() =>
+    Promise.resolve({
+        text: () => Promise.resolve(80805),
+    })
+)
+
+beforeEach(() => {
+    fetch.mockClear()
+    resetMeta({
+        config: {
+            greeting: 'Dzień dobry!',
+        },
+    })
+})
+
+test('processEvent adds properties', async () => {
+    // create a random event
+    const event0 = createEvent({ event: 'booking completed', properties: { amount: '20', currency: 'USD' } })
+
+    // must clone the event since `processEvent` will mutate it otherwise
+    const event1 = await processEvent(clone(event0), getMeta())
+    expect(event1).toEqual({
+        ...event0,
+        properties: {
+            ...event0.properties,
+            greeting: 'Dzień dobry!',
+            greeting_counter: 0,
+            random: 80805,
+        },
+    })
+
+    const event2 = await processEvent(clone(event0), getMeta())
+    expect(event2).toEqual({
+        ...event1,
+        properties: {
+            ...event1.properties,
+            greeting_counter: 1,
+        },
+    })
+
+    const event3 = await processEvent(clone(event0), getMeta())
+    expect(event3).toEqual({
+        ...event2,
+        properties: {
+            ...event2.properties,
+            greeting_counter: 2,
+        },
+    })
+})
+
+test('processEvent does not crash with identify', async () => {
+    // create a random event
+    const event0 = createIdentify()
+
+    // must clone the event since `processEvent` will mutate it otherwise
+    const event1 = await processEvent(clone(event0), getMeta())
+    expect(event1).toEqual({ ...event0, greeting: 'Dzień dobry!', greeting_counter: 0, random: 80805 })
+})
--- src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { hello } from '.'
-
-describe('hello', () => {
-    it('returns an informative message', () => {
-        expect(hello({ log: false })).toStrictEqual('This is where it all starts')
-    })
-})

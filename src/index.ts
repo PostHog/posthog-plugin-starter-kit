65c0f0b17b3037de7ea743923a6dc941abfc6c15
Michael Matloka <dev@twixes.com>
2021-01-18 13:02:32 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
-export interface HelloOptions {
-    log?: boolean
+import { Plugin } from '@posthog/plugin-scaffold'
+import type { RequestInfo, RequestInit, Response } from 'node-fetch'
+
+declare function fetch(url: RequestInfo, init?: RequestInit): Promise<Response>
+
+/** The famed Hello World plugin itself. */
+const helloWorld: Plugin = {
+    setupPlugin: async ({ config }) => {
+        console.log('Setting up the plugin!')
+        console.log(config)
+    },
+    processEvent: async (event, { config, cache }) => {
+        const counterValue = (await cache.get('greeting_counter', 0)) as number
+        cache.set('greeting_counter', counterValue + 1)
+        if (!event.properties) event.properties = {}
+        event.properties['greeting'] = config.greeting
+        event.properties['greeting_counter'] = counterValue
+        event.properties['random'] = await fetchTrulyRandomInteger()
+        return event
+    },
 }
 
-export function hello(options?: HelloOptions): string {
-    const messageForYou = 'This is where it all starts'
-    if (options?.log) console.log(messageForYou)
-    return messageForYou
-}
+export default helloWorld
 
-hello()
+/** Internal library function. */
+async function fetchTrulyRandomInteger(): Promise<number> {
+    const response = await fetch(
+        'https://www.random.org/integers/?num=1&min=1&max=1000000000&col=1&base=10&format=plain&rnd=new'
+    )
+    const integer = parseInt(await response.text())
+    return integer
+}
